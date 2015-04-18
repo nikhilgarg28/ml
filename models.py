bb7e71a70134edc261367e2d4ffc7b2cad00efd3
Nikhil Garg <nikhilgarg28@gmail.com>
2015-04-18 15:45:17 -0700

    linear regression: add normalization & docstring

--- models.py
+++ models.py
@@ -7,15 +7,39 @@ class Model(object):
 
 
 class LinearRegression(Model):
-    def __init__(self, learn_rate, max_iter, loss_fun):
+    def __init__(self, learn_rate, max_iter, loss_fun, normalize=True):
+        """Linear Regression model based on gradient descent.
+
+        Parameters
+        ----------
+        learn_rate : float
+            The rate at which model learns from gradient descent.
+
+        max_iter: int
+            The number of gradient descent iterations to try.
+
+        loss_fun : loss function object
+            An object that implements get_loss, get_gradient and get_f0
+            functions.
+
+        normalize : boolean, optional, default True
+            If features should be centered and normalized before regression.
+
+        """
         self._learn_rate = learn_rate
         self._max_iter = max_iter
         self._loss_fun = loss_fun
+        self._normalize = normalize
 
-        # we keep W to be 1d array
+        # feature centering parameters
+        self._means = None
+        self._scales = None
+
+        # W represents the learnt model, we keep it 1D array
         self._W = None
 
     def fit(self, X, Y, verbose=False):
+        self._computer_center_params(X)
         X = self._pre_process_input(X)
         N, F = X.shape
         loss_fun = self._loss_fun
@@ -29,8 +53,8 @@ class LinearRegression(Model):
                 print('-' * 30, i, '-' * 30)
             P = self._predict(W, X)
 
-            avg_loss = loss_fun.get_loss(P, Y)
             if verbose:
+                avg_loss = loss_fun.get_loss(P, Y)
                 print('Avg loss after %d iterations: %.3f' % (i, avg_loss))
 
             # gs is N sized 1d array containing loss fn gradients at each point
@@ -54,10 +78,17 @@ class LinearRegression(Model):
         return self._predict(self._W, X)
 
     def _pre_process_input(self, X):
+        if self._normalize:
+            X = (X - self._means) / self._scales
         N, F = X.shape
         X_ = np.ones((N, F + 1))
         X_[:, :-1] = X
         return X_
+
+    def _computer_center_params(self, X):
+        if self._normalize:
+            self._means = np.mean(X, axis=0)
+            self._scales = np.std(X, axis=0)
 
     def _predict(self, W, X):
         return np.dot(X, W)
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,29 +3,53 @@ from ml import loss
 
 import numpy as np
 from numpy import testing
-from sklearn.datasets import load_diabetes
+from sklearn.datasets import load_diabetes, load_boston
 
 class TestLinearRegression(object):
     def __init__(self):
-        dataset = load_diabetes()
-        self.X = dataset.data
-        self.Y = dataset.target
-        self.max_loss = 10000
+        self.alpha = 0.1
+        self.n_iter = 100
+        self.loss_fn = loss.L2()
 
     def test_fit(self):
-        alpha = 0.1
-        n_iter = 100
-        loss_fn = loss.L2()
-        lr = models.LinearRegression(alpha, n_iter, loss_fn)
-        lr.fit(self.X, self.Y)
-        P = lr.predict(self.X)
-        loss1 = loss_fn.get_loss(P, self.Y)
-        assert loss1 <= self.max_loss
+        dataset = load_diabetes()
+        X, Y = dataset.data, dataset.target
+        max_loss = 10000
+        lr = models.LinearRegression(self.alpha, self.n_iter, self.loss_fn)
+        lr.fit(X, Y)
+        P = lr.predict(X)
+        loss1 = self.loss_fn.get_loss(P, Y)
+        assert loss1 <= max_loss
 
         # also test that loss is decreasing with n_iter
-        n_iter *= 5
-        lr = models.LinearRegression(alpha, n_iter, loss_fn)
-        lr.fit(self.X, self.Y)
-        P = lr.predict(self.X)
-        loss2 = loss_fn.get_loss(P, self.Y)
-        assert loss2 <= loss1
+        n_iter = self.n_iter * 2
+        lr = models.LinearRegression(self.alpha, n_iter, self.loss_fn)
+        lr.fit(X, Y)
+        P = lr.predict(X)
+        loss2 = self.loss_fn.get_loss(P, Y)
+        assert loss2 <= max_loss
+
+    def test_normalize(self):
+        # boston dataset diverges unless features are normalized
+        dataset = load_boston()
+        X = dataset.data
+        Y = dataset.target
+
+        # if we don't normalize the data, model diverges
+        lr = models.LinearRegression(self.alpha, self.n_iter, self.loss_fn,
+                normalize=False)
+        lr.fit(X, Y)
+        P = lr.predict(X)
+        loss1 = self.loss_fn.get_loss(P, Y)
+        assert np.isnan(loss1)
+
+        # but if we do normalize, then nothing like this happens
+        max_loss = 50
+        lr = models.LinearRegression(self.alpha, self.n_iter, self.loss_fn,
+                normalize=True)
+        lr.fit(X, Y)
+
+        P = lr.predict(X)
+        loss2 = self.loss_fn.get_loss(P, Y)
+        assert not np.isnan(loss2)
+        assert loss2 <= max_loss
