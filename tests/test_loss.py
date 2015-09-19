74f495db08302633ee37b2832554137b0f2dee0c
Nikhil Garg <nikhilgarg28@gmail.com>
2015-09-19 14:57:46 -0700

    tests: replace naked assert with nose.tools.eq_

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,5 +1,7 @@
 from ml import loss
 
+from nose.tools import *
+
 import numpy as np
 from numpy import testing
 
@@ -12,23 +14,23 @@ class TestL2(object):
 
     def test_loss(self):
         # zero loss for perfect prediction, should work on both arrays & scalars
-        assert self.l2.get_loss(self.a, self.a) == 0
-        assert self.l2.get_loss(13.1, 13.1) == 0
+        eq_(self.l2.get_loss(self.a, self.a), 0)
+        eq_(self.l2.get_loss(13.1, 13.1), 0)
 
         # correct l2 loss otherwise
-        assert self.l2.get_loss(3, 0) == 9
-        assert self.l2.get_loss(0, 3) == 9
-        assert self.l2.get_loss(self.a, self.b) == 1
-        assert self.l2.get_loss(self.b, self.a) == 1
+        eq_(self.l2.get_loss(3, 0), 9)
+        eq_(self.l2.get_loss(0, 3), 9)
+        eq_(self.l2.get_loss(self.a, self.b), 1)
+        eq_(self.l2.get_loss(self.b, self.a), 1)
 
-        assert self.l2.get_loss(5, 1) == 16
-        assert self.l2.get_loss(5.7, 1.7) == 16
-        assert self.l2.get_loss(5.7, 1) == 4.7 ** 2
+        eq_(self.l2.get_loss(5, 1), 16)
+        eq_(self.l2.get_loss(5.7, 1.7), 16)
+        eq_(self.l2.get_loss(5.7, 1), 4.7 ** 2)
 
     def test_gradient(self):
         # zero gradient at minima
-        assert self.l2.get_gradient(1, 1) == 0
-        assert self.l2.get_gradient(9, 9) == 0
+        eq_(self.l2.get_gradient(1, 1), 0)
+        eq_(self.l2.get_gradient(9, 9), 0)
 
         # works for arrays too, returns array of same size
         testing.assert_array_almost_equal(
@@ -37,8 +39,8 @@ class TestL2(object):
         )
 
         # correct gradient otherwise
-        assert self.l2.get_gradient(9, 0) == 18
-        assert self.l2.get_gradient(0, 9) == -18
+        eq_(self.l2.get_gradient(9, 0), 18)
+        eq_(self.l2.get_gradient(0, 9), -18)
         testing.assert_array_almost_equal(
                 self.l2.get_gradient(self.a, self.b),
                 np.array([-2, -2, -2])
@@ -50,11 +52,9 @@ class TestL2(object):
 
     def test_f0(self):
         Y = np.array([1, 1, 1])
-        assert self.l2.get_f0(Y) == 1
-
-
+        eq_(self.l2.get_f0(Y), 1)
         Y = np.array([1, 2, 3, 4])
-        assert self.l2.get_f0(Y) == 2.5
+        eq_(self.l2.get_f0(Y), 2.5)
 
 
 class TestLog(object):
@@ -64,13 +64,13 @@ class TestLog(object):
         self.b = np.array([0.3, 0.2, 0.1])
 
     def test_loss(self):
-        assert self.lf.get_loss(0.3, 0) == -np.log(0.7)
-        assert self.lf.get_loss(0.3, 1) == -np.log(0.3)
+        eq_(self.lf.get_loss(0.3, 0), -np.log(0.7))
+        eq_(self.lf.get_loss(0.3, 1), -np.log(0.3))
         assert abs(self.lf.get_loss(self.a, self.b) - 1.70633504237) < 1e-6
 
     def test_gradient(self):
         # zero gradient at minima
-        assert self.lf.get_gradient(0.5, 0.5) == 0
+        eq_(self.lf.get_gradient(0.5, 0.5), 0)
 
         # works for arrays too, returns array of same size
         testing.assert_array_almost_equal(
@@ -89,8 +89,8 @@ class TestLog(object):
 
     def test_f0(self):
         Y = np.array([1, 1, 1])
-        assert self.lf.get_f0(Y) == 1
-
+        eq_(self.lf.get_f0(Y), 1)
 
         Y = np.array([1, 2, 3, 4])
-        assert self.lf.get_f0(Y) == 2.5
+        eq_(self.lf.get_f0(Y), 2.5)
+
